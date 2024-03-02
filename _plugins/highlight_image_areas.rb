3bd681cb45ca7fe60219fa2f3bb2ebaca0a247b5
amoamare <amoamare@users.noreply.github.com>
2024-03-02 13:28:17 -0700

    f

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -8,7 +8,8 @@
 module Jekyll
     class HighlightImageAreasBlock < Liquid::Block
 
-      def initialize(tag_name, markup)
+      def initialize(tag_name, markup, tokens)
+        super
         @attributes = {}
 
         @attributes['img'] = '';
@@ -26,20 +27,32 @@ module Jekyll
         #if @attributes['img'].nil?
         #   raise SyntaxError.new("You did not specify a directory for highlight_img_areas.")
         #end
-
-        super
-    end
+      end
+     
       def render(context)
         context.registers[:highlight_img_areas] ||= Hash.new(0)
 
         image = Dir.glob(@attributes['img'])
 
-        text = super
-        "<p>#{text} #{Time.now}</p>"
-       
+        ##text = super
+       ## "<p>#{text} #{Time.now}</p>"
+          "
+          <div class=\"container\">
+          <img class=\"image\" src=\"#{ image }\" alt=\"Background Image\">
+          {% if page.highlighted_areas %}
+            {% assign selected_areas = page.highlighted_areas | split: ',' %}
+            {% for area_id in selected_areas %}
+              {% assign area_info = site.data.highlight_areas | where: \"id\", area_id | first %}
+              {% if area_info %}
+              <div class=\"highlight\" style=\"top: {{ area_info.top }}%; left: {{ area_info.left }}%; width: {{ area_info.width }}%; height: {{ area_info.height }}%;\">
+              {{ area_info.id }}
+              </div>  
+              {% endif %}
+            {% endfor %}
+          {% endif %}
+        </div>
+          "
       end
-  
-    end
   end
   
   Liquid::Template.register_tag('highlight_img_areas', Jekyll::HighlightImageAreasBlock)
