7b64e287c3bd37062b207dd2e8b610ba1d1b56f1
amoamare <amoamare@users.noreply.github.com>
2024-03-02 21:46:12 -0700

    asdfsdfasdfsdfsadfsdaf

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -14,13 +14,14 @@ module Jekyll
     def initialize(tag_name, markup, tokens)
       puts "Markup: #{markup}"
       @attributes = {}
-      # Parse parameters
-      markup.scan(Liquid::TagAttributes) do |key, value|
-        @attributes[key] = value
-      end
-       
+
+      @attributes['img'] = '';
+      @attributes['areas'] = '';
       puts "Markup: #{@attributes['img']}"
-      
+      # Parse parameters
+          markup.scan(Liquid::TagAttributes) do |key, value|
+              @attributes[key] = value
+       
 
 
       #if @attributes['img'].nil?
@@ -30,49 +31,45 @@ module Jekyll
       super
     end
 
-    def render(context)      
-      context.registers[:highlight_img_areas] ||= Hash.new(0)
-      site = context.registers[:site]
-    
-      site_highlight_areas = site.data['highlight_areas']
-      image = @attributes['img']
-      
-      highlighted_areas = "0,1,3,4";
-    
-      output = <<~EOS
-      <div class="container">
-        <img class="image" src="#{image}" alt="Background Image">
-        {% if highlighted_areas %}
-          {% assign selected_areas = highlighted_areas | split: ',' %}
-          {% for area_id in selected_areas %}
-            {% assign area_info = site_highlight_areas | where: "id", area_id | first %}
-            {% if area_info %}
-              <div class="highlight" style="top: {{ area_info['top'] }}%; left: {{ area_info['left'] }}%; width: {{ area_info['width'] }}%; height: {{ area_info['height'] }}%;">
-                {{ area_info['id'] }}
-              </div>  
-            {% endif %}
-          {% endfor %}
-        {% endif %}
-      </div>
-      EOS
-    
-      # Parse the output string with Liquid to render any Liquid syntax
-      rendered_output = Liquid::Template.parse(output).render(context)
-    
-      # Return the rendered output
-      rendered_output
-    end
-    
+    def render(context)
+      image = Dir.glob(@attributes['img']).first
 
-    # Override blank? method to always return true, indicating that the block is blank
-    def blank?
-      false
+      puts "Markup: #{image}"
+      
+
+
+#  content = super
+
+output = <<~EOS
+<div class="container">
+  <img class="image" src="#{image}" alt="Background Image">
+  {% if highlighted_areas %}
+    {% assign selected_areas = highlighted_areas | split: ',' %}
+    {% for area_id in selected_areas %}
+      {% assign area_info = site.data.highlight_areas | where: "id", area_id | first %}
+      {% if area_info %}
+      <div class="highlight" style="top: {{ area_info.top }}%; left: {{ area_info.left }}%; width: {{ area_info.width }}%; height: {{ area_info.height }}%;">
+      {{ area_info.id }}
+      </div>  
+      {% endif %}
+    {% endfor %}
+  {% endif %}
+</div>
+EOS
+
+    # Parse the output string with Liquid to render any Liquid syntax
+    rendered_output = Liquid::Template.parse(output).render(context)
+
+    # Return the rendered output
+    rendered_output
     end
+
+  # Override blank? method to always return true, indicating that the block is blank
+  def blank?
+    false
   end
+end
+end
 end
 
 Liquid::Template.register_tag('highlight_img_areas', Jekyll::HighlightImageAreasBlock)
-
-Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
-  doc.output = Liquid::Template.parse(doc.output).render("site" => doc.site)
-end
