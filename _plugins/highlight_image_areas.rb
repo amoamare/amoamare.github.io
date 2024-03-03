8a8490677018ef16da889209c698ddf46ca51154
amoamare <amoamare@users.noreply.github.com>
2024-03-02 20:17:56 -0700

    asd

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -32,40 +32,36 @@ module Jekyll
     def render(context)      
       context.registers[:highlight_img_areas] ||= Hash.new(0)
       site = context.registers[:site]
-      doc = context.registers[:page]
-
+    
       site_highlight_areas = site.data['highlight_areas']
       image = @attributes['img']
-
-      puts "Markup: #{site_highlight_areas}"
       
       highlighted_areas = "0,1,3,4";
-
-#  content = super
-
-output = <<~EOS
-<div class="container">
-  <img class="image" src="#{image}" alt="Background Image">
-  {% if highlighted_areas %}
-    {% assign selected_areas = highlighted_areas | split: ',' %}
-    {% for area_id in selected_areas %}
-      {% assign area_info = #{site_highlight_areas} | where: "id", area_id | first %}
-      {% if area_info %}
-      <div class="highlight" style="top: {{ area_info.top }}%; left: {{ area_info.left }}%; width: {{ area_info.width }}%; height: {{ area_info.height }}%;">
-      {{ area_info.id }}
-      </div>  
-      {% endif %}
-    {% endfor %}
-  {% endif %}
-</div>
-EOS
-
-    # Parse the output string with Liquid to render any Liquid syntax
-    rendered_output = Liquid::Template.parse(output).render(context)
-
-    # Return the rendered output
-    rendered_output
+    
+      output = <<~EOS
+      <div class="container">
+        <img class="image" src="#{image}" alt="Background Image">
+        {% if highlighted_areas %}
+          {% assign selected_areas = highlighted_areas | split: ',' %}
+          {% for area_id in selected_areas %}
+            {% assign area_info = site_highlight_areas | where: "id", area_id | first %}
+            {% if area_info %}
+              <div class="highlight" style="top: {{ area_info['top'] }}%; left: {{ area_info['left'] }}%; width: {{ area_info['width'] }}%; height: {{ area_info['height'] }}%;">
+                {{ area_info['id'] }}
+              </div>  
+            {% endif %}
+          {% endfor %}
+        {% endif %}
+      </div>
+      EOS
+    
+      # Parse the output string with Liquid to render any Liquid syntax
+      rendered_output = Liquid::Template.parse(output).render(context)
+    
+      # Return the rendered output
+      rendered_output
     end
+    
 
   # Override blank? method to always return true, indicating that the block is blank
   def blank?
