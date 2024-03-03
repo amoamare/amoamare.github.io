f1d2e2b64433a818d1d5076698021b2406435282
amoamare <amoamare@users.noreply.github.com>
2024-03-02 23:52:06 -0700

    context redifned

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -7,26 +7,21 @@
 
 module Jekyll
   class HighlightImageAreasBlock < Liquid::Block
-    include Liquid::StandardFilters
+
+    include Liquid::StandardFilters    
+    Syntax = /(#{Liquid::QuotedFragment}+)?/
 
     def initialize(tag_name, markup, tokens)
       super
       @attributes = parse_attributes(markup)
-      @image = @attributes['img']
-      @highlighted_areas = "0,1,3,4"
-      @site_highlight_areas = context.registers[:site].data['highlight_areas'] || []
     end
 
     def render(context)
-      context['highlighted_areas'] = highlighted_areas
-      context['site_highlight_areas'] = site_highlight_areas
-      context['image'] = image
+      @context = context
+      output = read_template_file('highlight_img_areas.html')
+      rendered_output = Liquid::Template.parse(output).render(build_context)
 
-      output = File.read(File.join(context.registers[:site].config['source'], '_includes', 'highlight_img_areas.html'))
-      rendered_output = Liquid::Template.parse(output).render(context)
-
-      super
-      rendered_output
+      super + rendered_output
     end
 
     private
@@ -37,6 +32,18 @@ module Jekyll
         attributes[key] = value
       end
       attributes
+    end
+
+    def build_context
+      {
+        'image' => @attributes['img'],
+        'highlighted_areas' => '0,1,3,4',
+        'site_highlight_areas' => @context.registers[:site].data['highlight_areas'] || []
+      }
+    end
+
+    def read_template_file(filename)
+      File.read(File.join(@context.registers[:site].config['source'], '_includes', filename))
     end
   end
 end
