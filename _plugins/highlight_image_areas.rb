e90453ea1488cffaeb21ad375ca3172848a74ef1
amoamare <amoamare@users.noreply.github.com>
2024-03-02 23:48:17 -0700

    funny right

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -7,60 +7,36 @@
 
 module Jekyll
   class HighlightImageAreasBlock < Liquid::Block
-
     include Liquid::StandardFilters
-    Syntax = /(#{Liquid::QuotedFragment}+)?/
 
     def initialize(tag_name, markup, tokens)
-      puts "Markup: #{markup}"
-      @attributes = {}
-      # Parse parameters
-      markup.scan(Liquid::TagAttributes) do |key, value|
-        @attributes[key] = value
-      end      
       super
+      @attributes = parse_attributes(markup)
+      @image = @attributes['img']
+      @highlighted_areas = "0,1,3,4"
+      @site_highlight_areas = context.registers[:site].data['highlight_areas'] || []
     end
 
-    def render(context)      
-      context.registers[:highlight_img_areas] ||= Hash.new(0)      
-      site = context.registers[:site]    
-      site_highlight_areas = site.data['highlight_areas'] || []
-      
-      highlighted_areas = "0,1,3,4"
-      context['highlighted_areas'] = highlighted_areas      
-      context['site_highlight_areas'] = site_highlight_areas       
-      image = @attributes['img']
+    def render(context)
+      context['highlighted_areas'] = highlighted_areas
+      context['site_highlight_areas'] = site_highlight_areas
+      context['image'] = image
 
-      output = <<~HTML
-      <div class="highlight_image_areas_container">
-        <img class="img_highlight_image_areas" src="#{image}" alt="Background Image">
-        <div name="highlights">
-          {% if highlighted_areas %}
-            {% assign selected_areas = highlighted_areas | split: ',' %}
-            {% for area_id in selected_areas %}
-              {% assign area_info = site_highlight_areas | where: "id", area_id | first %}              
-              {% if area_info %}
-              <div class="highlight" style="top: {{ area_info.top }}%; left: {{ area_info.left }}%; width: {{ area_info.width }}%; height: {{ area_info.height }}%;">
-                {{ area_info.id }}
-              </div>  
-              {% endif %}
-            {% endfor %}        
-          {% endif %}
-        </div>
-      </div>
-      HTML
-
-      # Parse the output string with Liquid to render any Liquid syntax
+      output = File.read(File.join(context.registers[:site].config['source'], '_includes', 'highlight_img_areas.html'))
       rendered_output = Liquid::Template.parse(output).render(context)
 
-      # Return the rendered output
       super
       rendered_output
     end
 
-    # Override blank? method to always return true, indicating that the block is blank
-    def blank?
-      false
+    private
+
+    def parse_attributes(markup)
+      attributes = {}
+      markup.scan(Liquid::TagAttributes) do |key, value|
+        attributes[key] = value
+      end
+      attributes
     end
   end
 end
