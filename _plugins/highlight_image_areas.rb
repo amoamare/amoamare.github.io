1c9bbff286a989e732b5b869af77ac1db4fddc0c
amoamare <amoamare@users.noreply.github.com>
2024-03-02 21:22:26 -0700

    hahahaha

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -1,12 +1,6 @@
-
-#usage:
-#{% highlight_img_areas directory:images iterator:image filter:*.jpg sort:descending %}
-#   <img src="{{ image }}" />
-#{% highlight_img_areas %}
-
-
 module Jekyll
   class HighlightImageAreasBlock < Liquid::Block
+
     include Liquid::StandardFilters
     Syntax = /(#{Liquid::QuotedFragment}+)?/
 
@@ -23,30 +17,34 @@ module Jekyll
     
       site_highlight_areas = site.data['highlight_areas']
       
-      @highlighted_areas = "0,1,3,4";
+      highlighted_areas = "0,1,3,4"
       image = @attributes['img']
       
-      
-      puts "S: #{site_highlight_areas}"
-      puts "s: #{highlighted_areas}"
       
       output = <<~HTML
       <div class="container">
         <img class="image" src="#{image}" alt="Background Image">
-      
+        {% if highlighted_areas %}
+        {% assign selected_areas = highlighted_areas | split: ',' %}
+        {% for area_id in selected_areas %}
+          {% assign area_info = site_highlight_areas | where: "id", area_id | first %}
+          {% if area_info %}
+            <div class="highlight" style="top: {{ area_info.top }}%; left: {{ area_info.left }}%; width: {{ area_info.width }}%; height: {{ area_info.height }}%;">
+              {{ area_info.id }}
+            </div>  
+          {% endif %}
+        {% endfor %}
+      {% endif %}
 
       </div>
       HTML
-      
-      puts "s: #{output}"
     
       # Parse the output string with Liquid to render any Liquid syntax
       rendered_output = Liquid::Template.parse(output).render(context)
-      #rendered_output = Liquid::Template.parse(output).render('highlighted_areas' => highlighted_areas, 'context' => context)
+      
       # Return the rendered output
       rendered_output
     end
-        
   end
 end
 
