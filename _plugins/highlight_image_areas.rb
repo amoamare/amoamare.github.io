1644f17635de69c5de27fef9d710112551bdfd92
amoamare <amoamare@users.noreply.github.com>
2024-03-02 21:40:02 -0700

    asdf

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -5,30 +5,12 @@ module Jekyll
     Syntax = /(#{Liquid::QuotedFragment}+)?/
 
     def initialize(tag_name, markup, tokens)
-      super
-      @attributes = {}
-      markup.scan(Liquid::TagAttributes) do |key, value|
-        @attributes[key] = value    
-      end  
+  
     end
 
     def render(context)      
       context.registers[:highlight_img_areas] ||= Hash.new(0)
-      image = @attributes['img']
-      
-      
-      output = <<~HTML
-      <div class="container">
-        <img class="image" src="#{image}" alt="Background Image">
-     
-      </div>
-      HTML
-    
-      # Parse the output string with Liquid to render any Liquid syntax
-      rendered_output = Liquid::Template.parse(output).render(context)
-      
-      # Return the rendered output
-      rendered_output
+      output
     end
   end
 end
