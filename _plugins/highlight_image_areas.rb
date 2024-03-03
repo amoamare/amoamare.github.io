2dda5b918bdcd5ef86bd6922f38f464b334b4062
amoamare <amoamare@users.noreply.github.com>
2024-03-02 22:11:59 -0700

    maybe

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -35,7 +35,7 @@ module Jekyll
     
       site_highlight_areas = site.data['highlight_areas']
       
-      @highlighted_areas = "0,1,3,4";
+      highlighted_areas = "0,1,3,4";
 
       image = @attributes['img']
 
