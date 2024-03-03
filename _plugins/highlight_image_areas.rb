78757211e4ea20c76b2f7566828e5027ab7ccf78
amoamare <amoamare@users.noreply.github.com>
2024-03-02 21:44:27 -0700

    fffffadsf

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -1,3 +1,10 @@
+
+#usage:
+#{% highlight_img_areas directory:images iterator:image filter:*.jpg sort:descending %}
+#   <img src="{{ image }}" />
+#{% highlight_img_areas %}
+
+
 module Jekyll
   class HighlightImageAreasBlock < Liquid::Block
 
@@ -5,12 +12,61 @@ module Jekyll
     Syntax = /(#{Liquid::QuotedFragment}+)?/
 
     def initialize(tag_name, markup, tokens)
+      puts "Markup: #{markup}"
+      @attributes = {}
+      # Parse parameters
+      markup.scan(Liquid::TagAttributes) do |key, value|
+        @attributes[key] = value
+      end
+       
+      puts "Markup: #{@attributes['img']}"
+      
+
+
+      #if @attributes['img'].nil?
+      #   raise SyntaxError.new("You did not specify a directory for highlight_img_areas.")
+      #end
+      
       super
     end
 
     def render(context)      
       context.registers[:highlight_img_areas] ||= Hash.new(0)
-      super
+      site = context.registers[:site]
+    
+      site_highlight_areas = site.data['highlight_areas']
+      image = @attributes['img']
+      
+      highlighted_areas = "0,1,3,4";
+    
+      output = <<~EOS
+      <div class="container">
+        <img class="image" src="#{image}" alt="Background Image">
+        {% if highlighted_areas %}
+          {% assign selected_areas = highlighted_areas | split: ',' %}
+          {% for area_id in selected_areas %}
+            {% assign area_info = site_highlight_areas | where: "id", area_id | first %}
+            {% if area_info %}
+              <div class="highlight" style="top: {{ area_info['top'] }}%; left: {{ area_info['left'] }}%; width: {{ area_info['width'] }}%; height: {{ area_info['height'] }}%;">
+                {{ area_info['id'] }}
+              </div>  
+            {% endif %}
+          {% endfor %}
+        {% endif %}
+      </div>
+      EOS
+    
+      # Parse the output string with Liquid to render any Liquid syntax
+      rendered_output = Liquid::Template.parse(output).render(context)
+    
+      # Return the rendered output
+      rendered_output
+    end
+    
+
+    # Override blank? method to always return true, indicating that the block is blank
+    def blank?
+      false
     end
   end
 end
