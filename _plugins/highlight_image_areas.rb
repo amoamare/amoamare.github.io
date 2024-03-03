36008d9af01bb9a838cfcb48d1bde72bf4f9219b
amoamare <amoamare@users.noreply.github.com>
2024-03-02 20:22:27 -0700

    head hurts

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -41,10 +41,10 @@ module Jekyll
       output = <<~EOS
       <div class="container">
         <img class="image" src="#{image}" alt="Background Image">
-        {% if highlighted_areas %}
-          {% assign selected_areas = highlighted_areas | split: ',' %}
+        {% if #{highlighted_areas} %}
+          {% assign selected_areas = #{highlighted_areas} | split: ',' %}
           {% for area_id in selected_areas %}
-            {% assign area_info = site_highlight_areas | where: "id", area_id | first %}
+            {% assign area_info = #{site_highlight_areas} | where: "id", area_id | first %}
             {% if area_info %}
               <div class="highlight" style="top: {{ area_info['top'] }}%; left: {{ area_info['left'] }}%; width: {{ area_info['width'] }}%; height: {{ area_info['height'] }}%;">
                 {{ area_info['id'] }}
