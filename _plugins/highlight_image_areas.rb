aba6387ef2a4eb076361c4079484b849a0bcfef0
amoamare <amoamare@users.noreply.github.com>
2024-03-04 13:27:25 -0700

    sdd

--- _plugins/highlight_image_areas.rb
+++ _plugins/highlight_image_areas.rb
@@ -40,7 +40,6 @@ module Jekyll
       puts "before loop"
       # Loop through selected areas and build HTML for each
       selected_areas.each do |area_id|
-        puts "before loo1p #{|area| area['id']}"
         puts "before loo1p #{area_id['id']}"
         area_info = site_highlight_areas.find { |area| area['id'] == area_id }
         
