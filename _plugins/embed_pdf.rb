a393215dfdd3eea604b9368ad87b563dcc4823c6
amoamare <amoamare@users.noreply.github.com>
2024-03-14 21:48:41 -0600

    embed again

--- _plugins/embed_pdf.rb
+++ _plugins/embed_pdf.rb
@@ -10,12 +10,20 @@ Module Jekyll
       baseurl = context.registers[:site].config['baseurl']
       # Construct the full URL to the PDF file
       pdf_url = "#{baseurl}/assests/documents/#{@input}"
+
+      output = <<~HTML
+        <object title='#{pdf_url}' data='#{pdf_url}' type='application/pdf' width='700px' height='700px'>
+            <embed src='#{pdf_url}' width='700px' height='700px'>
+                <p>
+                    This browser does not support PDFs. Please download the PDF to view it: <a href='#{pdf_url}'>Download PDF</a>.
+                </p>
+            </embed>
+        </object>
+        HTML
+
+    Liquid::Template.parse(output).render(context)
+
     # Custom HTML for embedding PDF
-      "<object title='#{pdf_url}' data='#{pdf_url}' type='application/pdf' width='700px' height='700px'>
-        <embed src='#{pdf_url}' width='700px' height='700px'>
-          <p>This browser does not support PDFs. Please download the PDF to view it: <a href='#{pdf_url}'>Download PDF</a>.</p>
-        </embed>
-      </object>"
     end
   end
 end
