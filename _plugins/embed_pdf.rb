80857064a09a4496d80ef7aad74433f710d295dc
amoamare <amoamare@users.noreply.github.com>
2024-03-14 22:34:20 -0600

    align me gently

--- _plugins/embed_pdf.rb
+++ _plugins/embed_pdf.rb
@@ -15,20 +15,26 @@ module Jekyll
       width = "700px";
       height = "700px";
       output = <<~HTML
-        <!-- Display PDF using <object> -->
-        <object title="#{@input}" data="#{pdf_url}" type="application/pdf" width="#{width}" height="#{height}">
-        <!-- Display a download link if <object> is not supported -->
-        <p>This browser does not support displaying PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.</p>
-        
-        <!-- If <object> is not supported, try displaying PDF using <iframe> -->
-        <iframe title="#{@input}" src="#{pdf_url}" width="#{width}" height="#{height}" hidden>
-            <!-- If <iframe> is not supported, try displaying PDF using <embed> -->
-            <embed title="#{@input}" src="#{pdf_url}" width="#{width}" height="#{height}" hidden>
-                <!-- Display a download link if <embed> is not supported -->
-                <p>This browser does not support displaying PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.</p>
-            </embed>
-        </iframe>
-        </object>
+        <table width="100%" height="100%">
+            <tr>
+                <td align="center" valign="middle">
+                    <!-- Your PDF display code here -->
+                    <object title="#{@input}" data="#{pdf_url}" type="application/pdf" width="#{width}" height="#{height}">
+                        <!-- Display a download link if <object> is not supported -->
+                        <p>This browser does not support displaying PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.</p>
+                        
+                        <!-- If <object> is not supported, try displaying PDF using <iframe> -->
+                        <iframe title="#{@input}" src="#{pdf_url}" width="#{width}" height="#{height}" hidden>
+                            <!-- If <iframe> is not supported, try displaying PDF using <embed> -->
+                            <embed title="#{@input}" src="#{pdf_url}" width="#{width}" height="#{height}" hidden>
+                                <!-- Display a download link if <embed> is not supported -->
+                                <p>This browser does not support displaying PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.</p>
+                            </embed>
+                        </iframe>
+                    </object>
+                </td>
+            </tr>
+        </table>
         HTML
 
     Liquid::Template.parse(output).render(context)
