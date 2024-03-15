d542f49136e8ff6547824e16a96244bb0b02e17f
amoamare <amoamare@users.noreply.github.com>
2024-03-14 22:15:28 -0600

    code fix

--- _plugins/embed_pdf.rb
+++ _plugins/embed_pdf.rb
@@ -11,15 +11,17 @@ module Jekyll
       # Fetch the base URL of the site
       baseurl = context.registers[:site].config['baseurl']
       # Construct the full URL to the PDF file
-      pdf_url = "#{baseurl}/assests/documents/#{@input}"
+      pdf_url = "#{baseurl}/assets/documents/#{@input}"
 
       output = <<~HTML
-        <object title="#{pdf_url}" data="#{pdf_url}" type='application/pdf' width='700px' height='700px'>
+        <object title="#{@input}" data="#{pdf_url}" type='application/pdf' width='700px' height='700px'>
+         <iframe src='#{pdf_url}'>
             <embed src="#{pdf_url}" width='700px' height='700px'>
                 <p>
                     This browser does not support PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.
                 </p>
-            </embed>
+        
+            </iframe>
         </object>
         HTML
 
