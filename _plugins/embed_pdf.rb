9c7bdba01b24deb90b190ae915834dd8a6afdd35
amoamare <amoamare@users.noreply.github.com>
2024-03-14 22:26:36 -0600

    width and height

--- _plugins/embed_pdf.rb
+++ _plugins/embed_pdf.rb
@@ -12,17 +12,18 @@ module Jekyll
       baseurl = context.registers[:site].config['baseurl']
       # Construct the full URL to the PDF file
       pdf_url = "#{baseurl}/assets/documents/#{@input}"
-
+      width = "700px";
+      height = "700px";
       output = <<~HTML
         <!-- Display PDF using <object> -->
-        <object title="#{@input}" data="#{pdf_url}" type="application/pdf" width="700px" height="700px">
+        <object title="#{@input}" data="#{pdf_url}" type="application/pdf" width="#{width}" height="#{height}">
         <!-- Display a download link if <object> is not supported -->
         <p>This browser does not support displaying PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.</p>
         
         <!-- If <object> is not supported, try displaying PDF using <iframe> -->
-        <iframe src="#{pdf_url}" width="700px" height="700px" hidden>
+        <iframe title="#{@input}" src="#{pdf_url}" width="#{width}" height="#{height}" hidden>
             <!-- If <iframe> is not supported, try displaying PDF using <embed> -->
-            <embed src="#{pdf_url}" width="700px" height="700px" hidden>
+            <embed title="#{@input}" src="#{pdf_url}" width="#{width}" height="#{height}" hidden>
                 <!-- Display a download link if <embed> is not supported -->
                 <p>This browser does not support displaying PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.</p>
             </embed>
