dd37949334533d1624e363b00df5fac1bac6e4ff
amoamare <amoamare@users.noreply.github.com>
2024-03-14 22:21:10 -0600

    try agina

--- _plugins/embed_pdf.rb
+++ _plugins/embed_pdf.rb
@@ -14,14 +14,19 @@ module Jekyll
       pdf_url = "#{baseurl}/assets/documents/#{@input}"
 
       output = <<~HTML
-        <object title="#{@input}" data="#{pdf_url}" type='application/pdf' width='700px' height='700px'>
-         <iframe src='#{pdf_url}'>
-            <embed src="#{pdf_url}" width='700px' height='700px'>
-                <p>
-                    This browser does not support PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.
-                </p>
+        <!-- Display PDF using <object> -->
+        <object title="#{@input}" data="#{pdf_url}" type="application/pdf" width="700px" height="700px">
+        <!-- Display a download link if <object> is not supported -->
+        <p>This browser does not support displaying PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.</p>
         
-            </iframe>
+        <!-- If <object> is not supported, try displaying PDF using <iframe> -->
+        <iframe src="#{pdf_url}" width="700px" height="700px" hidden>
+            <!-- If <iframe> is not supported, try displaying PDF using <embed> -->
+            <embed src="#{pdf_url}" width="700px" height="700px" hidden>
+                <!-- Display a download link if <embed> is not supported -->
+                <p>This browser does not support displaying PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.</p>
+            </embed>
+        </iframe>
         </object>
         HTML
 
