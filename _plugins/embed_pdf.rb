fc59c7013c04ddbf874b1a25123ee613886be2f4
amoamare <amoamare@users.noreply.github.com>
2024-03-14 22:05:14 -0600

    fixed

--- _plugins/embed_pdf.rb
+++ _plugins/embed_pdf.rb
@@ -14,26 +14,18 @@ module Jekyll
       pdf_url = "#{baseurl}/assests/documents/#{@input}"
 
       output = <<~HTML
-        <div class="highlight_image_areas_container">
-        <embed srt="">
-        </embed>
-            <img class="img_highlight_image_areas" alt="img" src="#{region_data['Image']}">
-            <div name="highlights">
-            #{area_html.join("\n")}
-            </div>
-        </div>
-        <object title='#{pdf_url}' data='#{pdf_url}' type='application/pdf' width='700px' height='700px'>
-            <embed src='#{pdf_url}' width='700px' height='700px'>
+        <object title="#{pdf_url}" data="#{pdf_url}" type='application/pdf' width='700px' height='700px'>
+            <embed src="#{pdf_url}" width='700px' height='700px'>
                 <p>
-                    This browser does not support PDFs. Please download the PDF to view it: <a href='#{pdf_url}'>Download PDF</a>.
+                    This browser does not support PDFs. Please download the PDF to view it: <a href="#{pdf_url}">Download PDF</a>.
                 </p>
             </embed>
         </object>
         HTML
 
-        Liquid::Template.parse(output).render(context)
+    Liquid::Template.parse(output).render(context)
     end
-    
+
   end
 end
 
